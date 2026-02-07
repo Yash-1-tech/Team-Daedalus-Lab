71d590983333270e6cadeaa053533b401fcfbeb1
Tkd2211 <123975589+Tkd2211@users.noreply.github.com>
2026-02-07 21:11:34 +0530

    Initial frontend setup

---
+++ lab-website/frontend/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main style={{ 
+      height: "100vh", 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center",
+      fontSize: "2rem"
+    }}>
+      TDL lab - Site under development
+    </main>
+  );
+}
+
